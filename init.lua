8ec54fae79c50adb40cf1ccf0d37cdd723810605
alkaabi1972 <alkaabi1972@gmail.com>
2025-12-31 23:12:18 -0700

    add mason-lspconfig

--- init.lua
+++ init.lua
@@ -29,15 +29,25 @@ require("lazy").setup({
   spec = {
     -- add your plugins here
     {
-      "mason-org/mason.nvim",opts = {
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-          }
-        }
-      }
+	    "mason-org/mason.nvim",opts = {
+		    ui = {
+			    icons = {
+				    package_installed = "✓",
+				    package_pending = "➜",
+				    package_uninstalled = "✗"
+			    }
+		    }
+	    }
+    },
+    {
+	    "mason-org/mason-lspconfig.nvim",
+	    opts = {
+		    ensure_installed = { "lua_ls", "rust_analyzer" },
+	    },
+	    dependencies = {
+		    { "mason-org/mason.nvim", opts = {} },
+		    "neovim/nvim-lspconfig",
+	    },
     }
   },
   -- Configure any other settings here. See the documentation for more details.
